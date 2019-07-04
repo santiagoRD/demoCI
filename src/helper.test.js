29980f5c3e1964259505c16b2f19a17f939846e7
Santiago <santi_diaz17@hotmail.com>
2019-07-04 07:38:53 -0500

    prueba ok

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,6 +1,6 @@
 describe("probaar metodo suma", () => {
   const sum = require("./helper");
   test("add 1 + 2 equal 3", () => {
-    expect(sum(3, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
   });
 });
