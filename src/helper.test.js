1d8bd40fc67cd92d5ddf107aa93c0672a369abd6
Santiago <santi_diaz17@hotmail.com>
2019-07-04 07:02:18 -0500

    prueba para fallar3

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,10 +1,6 @@
 describe("probaar metodo suma", () => {
   const sum = require("./helper");
   test("add 1 + 2 equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-
-  test("add 1 + 2 equal 5", () => {
-    expect(sum(2, 2)).toBe(3);
+    expect(sum(3, 2)).toBe(3);
   });
 });
